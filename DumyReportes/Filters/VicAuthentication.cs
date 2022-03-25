de2ae672a1aa3a82641ea22c150ee973c205d715
progjun2 <progjun2@grupomarves.net>
2022-03-24 18:14:36 -0600

    alv2

--- DumyReportes/Filters/VicAuthentication.cs
+++ DumyReportes/Filters/VicAuthentication.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 
 namespace DumyReportes.Filters
--- DumyReportes/Util/TokenValidatorHelper.cs
+++ DumyReportes/Util/TokenValidatorHelper.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,39 @@ namespace DumyReportes.Util
 {
     public class TokenValidatorHelper
     {
+        public bool IsValidToken(string token)
+        {
+            var secretKey = ConfigurationManager.AppSettings["JWT_SECRET_KEY"];
+            var audienceToken = ConfigurationManager.AppSettings["JWT_AUDIENCE_TOKEN"];
+            var issuerToken = ConfigurationManager.AppSettings["JWT_ISSUER_TOKEN"];
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
 
+            SecurityToken securityToken;
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            TokenValidationParameters validationParameters = new TokenValidationParameters()
+            {
+                ValidAudience = audienceToken,
+                ValidIssuer = issuerToken,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                LifetimeValidator = this.LifetimeValidator,
+                IssuerSigningKey = securityKey
+            };
+
+            tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+
+            
+
+        }
+
+        public bool LifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            if (expires != null)
+            {
+                if (DateTime.UtcNow < expires) return true;
+            }
+            return false;
+        }
 
     }
 }
